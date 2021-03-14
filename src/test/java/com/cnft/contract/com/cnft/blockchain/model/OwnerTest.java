6e0479bf81c3484eac97f5b4fe4eb7ddb7542dfd
shizhiguo <shizhiguo@meituan.com>
2021-03-14 19:59:48 +0800

    update

--- src/test/java/com/cnft/contract/com/cnft/blockchain/model/OwnerTest.java
+++ src/test/java/com/cnft/contract/com/cnft/blockchain/model/OwnerTest.java
@@ -12,12 +12,14 @@ import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Event;
 import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.RemoteFunctionCall;
 import org.web3j.protocol.core.methods.request.EthFilter;
 import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.Transaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.ReadonlyTransactionManager;
 import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.DefaultGasProvider;
@@ -30,14 +32,25 @@ public class OwnerTest {
     private static final int EVENT_COUNT = 5;
     private static final int TIMEOUT_MINUTES = 1;
 
+    private static Credentials TEST;
 
-    @Test
-    public void name() {
-
+    static {
+        Credentials.create("0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e", "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199");
     }
 
     @Test
-    public void changeOwner() {
+    public void deploy() throws Exception {
+        Owner owner = Owner.deploy(EthClient.getWeb3jClient(), TEST, new DefaultGasProvider()).send();
+        String contractAddress = owner.getContractAddress();
+        System.out.println("合约地址==>" + contractAddress);
+    }
+
+    @Test
+    public void changeOwner() throws Exception {
+        String contractAddr = "0xF8652d733ed090014e256fA3300e32F63bEa5E47";
+        Owner owner = Owner.load(contractAddr, EthClient.getWeb3jClient(), TEST, new DefaultGasProvider());
+        TransactionReceipt txReceipt = owner.changeOwner("0xf82f96aeD5F343545CaCc393F33BFE8E006f6928").send();
+        System.out.println("交易收据==>" + txReceipt);
     }
 
     @Test
@@ -85,44 +98,5 @@ public class OwnerTest {
             System.out.println("收到一条事件==>" + log.getTransactionHash());
             System.out.println(new ObjectMapper().writeValueAsString(log));
         }
-    }
-
-    @Test
-    public void logFlowable2() throws Exception {
-        Web3j web3jClient = EthClient.getWeb3jClient_MainNet();
-        EthFilter ethFilter = new EthFilter(
-                DefaultBlockParameterName.EARLIEST,
-                DefaultBlockParameterName.LATEST,
-                Arrays.asList("0xdAC17F958D2ee523a2206206994597C13D831ec7")
-        );
-        Event event = new Event("Transfer", Arrays.asList(
-                new TypeReference<Address>() {
-                }, new TypeReference<Address>() {
-                }, new TypeReference<Uint256>() {
-                })
-        );
-        String encode = EventEncoder.encode(event);
-        ethFilter.addSingleTopic(encode);
-        Flowable<Log> logFlowable = web3jClient.ethLogFlowable(ethFilter);
-        run(logFlowable);
-    }
-
-    private <T> void run(Flowable<T> flowable) throws Exception {
-        CountDownLatch countDownLatch = new CountDownLatch(EVENT_COUNT);
-        CountDownLatch completedLatch = new CountDownLatch(EVENT_COUNT);
-
-        Disposable subscription =
-                flowable.subscribe(
-                        x -> countDownLatch.countDown(),
-                        Throwable::printStackTrace,
-                        completedLatch::countDown);
-
-        countDownLatch.await(TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        subscription.dispose();
-        completedLatch.await(1, TimeUnit.SECONDS);
-
-        System.out.println(
-                "CountDownLatch=" + countDownLatch.getCount() + ", CompletedLatch=" + completedLatch.getCount());
-        Assert.assertTrue(subscription.isDisposed());
     }
 }
